3097c649cd992cf40844d71150e1154b724dfbc4
Rəsul Əliyev <resul.eliyev2000@mail.ru>
2021-07-24 09:15:37 +0400

    Few Changes

--- src/Components/About/AboutText.jsx
+++ src/Components/About/AboutText.jsx
@@ -3,7 +3,9 @@ const AboutText = () => {
   return (
     <div className={styles.textWrapper}>
       <h1 className={styles.heading}>Lorem, ipsum dolor.</h1>
-      <p className={styles.paragraph}>
+      <div className={styles.availableWrapper}></div>
+      <h1 className={styles.available}>Currently available</h1>
+      <p className={styles.paragraphBig}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
         omnis, natus enim dolore corrupti assumenda veritatis distinctio illo
         aliquam? Fugit, consectetur sit dolorum, aperiam repellat dignissimos ab
@@ -11,15 +13,15 @@ const AboutText = () => {
         non iusto veniam architecto cum reprehenderit recusandae. Ullam corrupti
         modi necessitatibus possimus cumque.
       </p>
-      <p className={styles.paragraph2}>
+      <p className={styles.paragraphMid}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos dolores
         provident rerum necessitatibus quas ullam optio suscipit, possimus est
         quia.
       </p>
       <span className={styles.divider}></span>
-      <p className={styles.paragraph3}>rasul.mail.00@mail.com</p>
+      <p className={styles.paragraphMail}>rasul.mail.00@mail.com</p>
       <span className={styles.divider2}></span>
-      <p className={styles.paragraph4}>
+      <p className={styles.paragraphSocial}>
         <a href="#">Instagram</a> <a href="#">Twitter</a>{" "}
         <a href="#">Linkedin</a> <a href="#">FaceBook</a>
       </p>
--- src/Components/Contents/Logo/Logo.jsx
+++ src/Components/Contents/Logo/Logo.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
 import { ReactComponent as LogoCircle } from "./LogoCircle.svg";
 import { ReactComponent as LogoLines } from "./LogoLines.svg";
 const Logo = () => {
   return (
-    <>
+    <Link to="/">
       <LogoCircle className={`${styles.logo} ${styles.circle}`} />
       <LogoLines className={`${styles.logo} ${styles.lines}`} />
-    </>
+    </Link>
   );
 };
 
